7aa989ea5022ac38866f09f20b65e2c4782782bd
vijaypatil2003 <vijayptl0106email@example.com>
2025-10-29 00:33:13 +0530

    Added admin folder

---
+++ admin/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Add from './pages/Add'
+import Orders from './pages/Orders'
+import List from './pages/list'
+
+function App() {
+
+  return (
+    <>
+    <Add/>
+    <List/>
+    <Orders/>
+    </>
+  )
+}
+
+export default App
